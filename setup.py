d233873fca59c26bd961e0bc58b51552fbe95cc5
Vladimir Bataev <vbataev@nvidia.com>
2023-12-20 17:01:24 +0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="min_rnnt",
+    version="0.0.1",
+    description="Min RNNT for experiments",
+    author="Vladimir Bataev",
+    author_email="artbataev@gmail.com",
+    url="https://github.com/artbataev/uol_final",
+    packages=find_packages(),
+    license="Apache-2.0",
+)
