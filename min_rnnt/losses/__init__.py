b3575941287bf30596462e14484cd3b6f304262d
Vladimir Bataev <vbataev@nvidia.com>
2024-02-05 23:04:27 +0400

    Add loss to common imports

--- min_rnnt/losses/__init__.py
+++ min_rnnt/losses/__init__.py
@@ -1,5 +1,11 @@
+from min_rnnt.losses.bypass_ml_transducer import GraphBypassMultiLevelTransducerLoss
 from min_rnnt.losses.bypass_transducer import GraphBypassTransducerLoss
 from min_rnnt.losses.star_transducer import GraphStarTransducerLoss
 from min_rnnt.losses.target_robust_transducer import GraphTargetRobustTransducerLoss
 
-__all__ = ["GraphStarTransducerLoss", "GraphTargetRobustTransducerLoss", "GraphBypassTransducerLoss"]
+__all__ = [
+    "GraphStarTransducerLoss",
+    "GraphTargetRobustTransducerLoss",
+    "GraphBypassTransducerLoss",
+    "GraphBypassMultiLevelTransducerLoss",
+]
