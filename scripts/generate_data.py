cb8ff0ae616c160b00f40ac8ab9d5546f3c9b1bb
Vladimir Bataev <vbataev@nvidia.com>
2024-03-09 21:25:53 +0400

    Add comments

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -6,10 +6,14 @@ from pathlib import Path
 from nemo.collections.asr.parts.utils.manifest_utils import read_manifest, write_manifest
 from tqdm.auto import tqdm
 
-RANDOM_SEED = 42
+RANDOM_SEED = 42  # random seed for reproducibility
 
 
 def modify_text(text: str, del_prob: float, sub_prob: float, ins_prob: float, vocab: list[str]):
+    """
+    Modify text, for each word using deletion, substitution or insertion (after word).
+    For substitution and insertion the word is randomly chosen from the vocab.
+    """
     words = text.split()
     modified_sentence: list[str] = []
     for word in words:
@@ -37,9 +41,15 @@ def corrupt_manifest(
     ins_prob: float = 0.0,
     clean_prob: float = 0.0,
 ):
+    """
+    Create corrupted transcript using random deletions, substitutions and/or insertions.
+    """
+    # sanity check for arguments: probability should be in [0.0, 1.0] range
     for prob, title in zip((del_prob, sub_prob, ins_prob, clean_prob), ("del", "sub", "ins", "clean")):
         assert 0.0 <= prob <= 1.0, f"{title} should be in [0.0, 1.0] range"
 
+    # read manifest, preserve uttid for speed-perturbed manifests
+    # (all records for each uttid will be modified in the same way)
     records = read_manifest(src_path)
     uttid2text = dict()
     for record in records:
@@ -48,12 +58,13 @@ def corrupt_manifest(
         if uttid not in uttid2text:
             uttid2text[uttid] = record["text"]
         else:
-            assert uttid2text[uttid] == record["text"]
+            assert uttid2text[uttid] == record["text"]  # check the text for uttid matches
 
     uttids = list(uttid2text.keys())
     vocab: list[str] = list(set(itertools.chain.from_iterable((text.split() for text in uttid2text.values()))))
     random_generator = random.Random(RANDOM_SEED)
     random_generator.shuffle(uttids)
+    # modify only (1-clean_prob) part of utterances
     num_modify = int(len(uttids) * (1 - clean_prob))
     for uttid in tqdm(uttids[:num_modify]):
         uttid2text[uttid] = modify_text(
@@ -75,12 +86,14 @@ def corrupt_manifest(
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src-path", type=Path, required=True)
-    parser.add_argument("--dst-path", type=Path, required=True)
-    parser.add_argument("--del-prob", type=float, default=0.0)
-    parser.add_argument("--sub-prob", type=float, default=0.0)
-    parser.add_argument("--ins-prob", type=float, default=0.0)
-    parser.add_argument("--clean-prob", type=float, default=0.0)
+    parser.add_argument("--src-path", type=Path, required=True, help="Source manifest path (NeMo format)")
+    parser.add_argument("--dst-path", type=Path, required=True, help="Target manifest path (NeMo format)")
+    parser.add_argument("--del-prob", type=float, default=0.0, help="Probability of deleting words from transcripts")
+    parser.add_argument("--sub-prob", type=float, default=0.0, help="Probability of substituting words in transcripts")
+    parser.add_argument(
+        "--ins-prob", type=float, default=0.0, help="Probability of adding random words to transcripts"
+    )
+    parser.add_argument("--clean-prob", type=float, default=0.0, help="Non-corrupted part of the dataset")
     args = parser.parse_args()
 
     corrupt_manifest(
